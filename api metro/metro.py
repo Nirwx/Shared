f8d1638ed3777c0bb0c4c719a0f7cd5b4fdd4cb6
Nirwx <33969483+Nirwx@users.noreply.github.com>
2019-11-12 17:20:53 +0100

    Update metro.py

--- api metro/metro.py
+++ api metro/metro.py
@@ -7,9 +7,9 @@ import json
 from bson.json_util import dumps
 
 
-
+# Fetch and Store information from the API
 class MetroApiOperations:
-
+#Fecth version information and build the URL accordingly if version has changed since the previous check
     def __init__(self):
 
         self.mongoconnection = MongoConnection()
@@ -25,7 +25,7 @@ class MetroApiOperations:
             self.d = self.build_app_dict(req.json(), dataAPI["storeHash"], dataAPI["version"], store_url)
             self.mongoconnection.insert_one('store_info', self.d)
 
-
+#Structure Data to be stored
     def build_app_dict(self, appStoreData, storeHash, storeVersion, store_url):
 
         d = defaultdict(dict)
@@ -49,7 +49,7 @@ class MetroApiOperations:
         return d
 
 
-    # Test URL status code for the entire Apps Dictionnary
+    # Test URL status code for the entire Apps Dictionnary and write status code in DB
     def test_response(self):
 
         del self.d["_id"]
@@ -63,28 +63,28 @@ class MetroApiOperations:
         self.mongoconnection.insert_one('http_response', self.d)
         return self.d["data"]
 
-
+#Fetch Status Data and structure to display in Grafana
 class getResponseData():
 
-    def __init__(self):
+    def structure_data(self):
         mongoco = MongoConnection()
         now = datetime.now()
-        date = datetime.now() - timedelta(days = 7)
-        self.d = {}
+        date = datetime.now() - timedelta(
+            days=30)  # placeholder, define date of data to poll (and display on Grafana) Tweak to extend/shrink date range in return response
+        d = defaultdict(dict)
+        y = 0
         for doc in mongoco.find_time_range('http_response', date, now):
-            self.d.update(doc)
-        print(self.d)
-        #self.d = mongoco.find_one('http_response')
+            for i in doc["data"]:
+                d["data"][str(y)] = doc["data"][str(i)]
+                y += 1
 
-
-    def structure_data(self):
         i = 0
         rows = ''
-        while i < len(self.d["data"]):
-            time = self.d["data"][str(i)]["last_check"]
-            app_name = self.d["data"][str(i)]["name"]
-            status = self.d["data"][str(i)]["status"]
-            if i+1 == len(self.d["data"]):
+        while i < len(d["data"]):
+            time = d["data"][str(i)]["last_check"]
+            app_name = d["data"][str(i)]["name"]
+            status = d["data"][str(i)]["status"]
+            if i + 1 == len(d["data"]):
                 row = '[{},"{}",{}]'.format(time, app_name, status)
             else:
                 row = '[{},"{}",{}],\n'.format(time, app_name, status)
@@ -103,7 +103,4 @@ class getResponseData():
             \"type\":\"table\"
           }
         ]'''
-
-
-    #test_response(global_dict)
-
+        return grafana_json
